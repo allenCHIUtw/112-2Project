1ddeebdb7e280a07753a7148e5012345f079146b
allenSLQ <allen@abin.com.tw>
2024-04-22 15:27:34 +0800

    HW7

---
+++ HW7-Mbed-DSP-programming/main.cpp
@@ -0,0 +1,24 @@
+#include "mbed.h"
+#include"stm32l475e_iot01_magneto.h"
+#include "stm32l475e_iot01_psensor.h"
+#include "stm32l475e_iot01_hsensor.h"
+#include "stm32l475e_iot01_accelero.h"
+
+void set_sensor()
+{
+   
+}
+void cauculate()
+{
+  
+}
+
+// main() runs in its own thread in the OS
+int main()
+{
+
+    while (true) {
+     
+    }
+}
+
